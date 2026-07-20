e16327bc36e1c0a0595124eb85b3c206dee6feba
jicodex <info.jicodex@gmail.com>
2026-07-21 05:06:58 +0600

    all

--- assets/js/script.js
+++ assets/js/script.js
@@ -136,6 +136,38 @@ $('.spacial_kalo_slider_main').slick({
         }
     ]
 });
+$('.spacial_two_over_card_slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    infinite: true,
+
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
 
 
 // ========= File Uploaded Input JS 
